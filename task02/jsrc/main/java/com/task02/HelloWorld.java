2ee380af4c67ea64045fcdd2d24cac54eabcd793
vivek_shrivastav <vivek_shrivastav@epam.com>
2024-05-31 18:39:41 +0530

    task02

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -21,8 +21,8 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
 
-@LambdaHandler(lambdaName = "hello_world", roleName = "hello_world-role", layers = {"sdk-layer"}, runtime = DeploymentRuntime.JAVA17, architecture = Architecture.ARM64, logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED)
-@LambdaLayer(layerName = "sdk-layer", libraries = {"lib/commons-lang3-3.14.0.jar", "lib/gson-2.11.0.jar"}, runtime = DeploymentRuntime.JAVA17, architectures = {Architecture.ARM64}, artifactExtension = ArtifactExtension.ZIP)
+@LambdaHandler(lambdaName = "hello_world", roleName = "hello_world-role", runtime = DeploymentRuntime.JAVA17, architecture = Architecture.ARM64, logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED)
+//@LambdaLayer(layerName = "sdk-layer", libraries = {"lib/commons-lang3-3.14.0.jar", "lib/gson-2.11.0.jar"}, runtime = DeploymentRuntime.JAVA17, architectures = {Architecture.ARM64}, artifactExtension = ArtifactExtension.ZIP)
 @LambdaUrlConfig(authType = AuthType.NONE, invokeMode = InvokeMode.BUFFERED)
 public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
 
